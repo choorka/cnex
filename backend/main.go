3645b47fd881fc833ddfb2357ec19cf240cb1fa5
choorka <xcom6@mail.ru>
2025-06-07 18:19:27 +0700

    cors3

--- backend/main.go
+++ backend/main.go
@@ -6,20 +6,21 @@ import (
 	"net/http"
 
 	"github.com/choorka/backend/telegram"
-	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusOK)
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
 			return
 		}
-		c.Next()
-	}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func latestPostHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,7 +35,11 @@ func latestPostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/latest", latestPostHandler)
-    fmt.Println("Сервер запущен")
-    http.ListenAndServe(":8080", CORSMiddleware())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/latest", latestPostHandler)
+
+	handler := CORSMiddleware(mux)
+
+	fmt.Println("Сервер запущен")
+	http.ListenAndServe(":8080", handler)
 }
